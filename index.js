a4edfc3ed5a07254130bb15cc7f6386999f24472
Nathan Kern <natjkern@gmail.com>
2018-02-24 23:15:43 -0600

    killed extra logging

--- index.js
+++ index.js
@@ -10,7 +10,6 @@ const removeObject = (index, key) => {
 // Takes an the Algolia index and data to be added or updated to
 const upsertObject = ( index, data ) => {
   // then it adds or updates it
-  console.log('index:', index);
   return index.saveObject(data, (err, content) => {
     if (err) throw err
     console.log(`Document ${data.objectID} Updated in Algolia Index `)
@@ -19,11 +18,8 @@ const upsertObject = ( index, data ) => {
 
 // Takes an Algolia index and a Firestore event and uses event data to keep them in sync
 exports.syncAlgoliaWithFirestore = (index, event) => {
-  console.log('event', event);
   const data = event.data._fieldsProto ? event.data.data() : null; // extract data from Firestore event
-  console.log('data', data);
   const key = event.params.id;      // gets the id of the document changed
-  console.log("key", key);
   // If no data then it was a delete event
   if (!data) {
     // so delete the document from Algolia index
